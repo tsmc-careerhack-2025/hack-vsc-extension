e8fb0cad9077e44aa6612d2dc27550d444c8f78f
Jian-Lin Li <r12944035@ntu.edu.tw>
2025-02-14 18:48:23 +0800

    feat: add web api access function

---
+++ src/utils/web-api.ts
@@ -0,0 +1,30 @@
+const API = "http://35.209.96.156";
+
+export interface ApiResponse {
+    success: boolean;
+    message: string;
+}
+
+export async function postData<T>(endPoint: string, data: any): Promise<T> {
+
+    try {
+        const response = await fetch(`${API}/${endPoint}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result: any = await response.json();
+
+        return result;
+    } catch (error) {
+        console.error('Error posting data:', error);
+        throw error;
+    }
+}
